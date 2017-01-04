e8184e6ad6ddedf159a39d05b180e0027b3e292a
aewebb80 <tug41380@temple.edu>
2017-01-04 18:09:30 -0500

    Testing dependencies with Travis CI

--- np_test.py
+++ np_test.py
@@ -3,3 +3,8 @@ import numpy as np
 test_list = [2,4,6,8]
 
 print np.average(test_list)
+
+import dendropy
+
+test_tree = dendropy.Tree.get_from_string("(A,(B,(C,D)))", "newick")
+test_tree.print_plot()
